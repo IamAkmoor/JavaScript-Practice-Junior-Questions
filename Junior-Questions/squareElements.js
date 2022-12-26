4a863110d00ee520fd74bc760ab68c38f8702456
Akmoor Abdirashid <akmoor.abdirashid.ch@gmail.com>
2022-12-26 12:21:25 +0400

    Added new file Junior-Questions and add practice question squareElements

---
+++ Junior-Questions/squareElements.js
@@ -0,0 +1,18 @@
+// squareElements
+
+// Write a function called "squareElements".
+
+// Given a array of numbers, "squareElements" should return a new array where each element is the square of the element of the given array.
+
+
+function squareElements(arr) {
+    // your code here
+  let newArr = []
+  for(let i in arr){
+    newArr.push(Math.pow(arr[i], 2))
+  }
+  return newArr
+}
+
+var output = squareElements([1, 2, 3]);
+console.log(output); // --> [1, 4, 9]
